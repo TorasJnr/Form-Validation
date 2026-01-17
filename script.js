eb5b2494f233b9bd614002b2b714aee5c452045e
TorasJnr <rasheedidris416@gmail.com>
2026-01-17 23:24:33 +0100

    Js added

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ var input = document.querySelectorAll("input");
     var error = document.querySelectorAll(".error");
     var click = document.querySelectorAll(".click");
     var click1 = document.querySelectorAll(".click1");
+
+
+// git add .
+// git commit -m "Js added"
+// git push
