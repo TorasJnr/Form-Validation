55f2b5d702632eaaafe363f21fe7d3d178a20848
TorasJnr <rasheedidris416@gmail.com>
2026-01-17 23:08:45 +0100

    Js added

---
+++ script.js
@@ -0,0 +1,5 @@
+var input = document.querySelectorAll("input");
+    input[0].focus()
+    var error = document.querySelectorAll(".error");
+    var click = document.querySelectorAll(".click");
+    var click1 = document.querySelectorAll(".click1");
