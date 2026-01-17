dcdf59406b34e790d827ee228698635710c1268b
TorasJnr <rasheedidris416@gmail.com>
2026-01-17 23:27:45 +0100

    Js added

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var input = document.querySelectorAll("input");
     var click = document.querySelectorAll(".click");
     var click1 = document.querySelectorAll(".click1");
 
+    function validate (){
+        if( input[0].value.trim() == ""){
+            input[0].focus()
+            input[0].style.border = "1px solid red";
+            error[0].style.display = "block"
+            click[0].style.display = "block"
+            click1[0].style.display = "none"
+            return false;
+        }
 
 // git add .
 // git commit -m "Js added"
