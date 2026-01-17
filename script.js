ff0b72b6dee2f6fec98c7c759866098540587aff
TorasJnr <rasheedidris416@gmail.com>
2026-01-17 23:34:35 +0100

    Js added

--- script.js
+++ script.js
@@ -4,7 +4,49 @@ var input = document.querySelectorAll("input");
     var click = document.querySelectorAll(".click");
     var click1 = document.querySelectorAll(".click1");
 
-    
+    function validate (){
+        if( input[0].value.trim() == ""){
+            input[0].focus()
+            input[0].style.border = "1px solid red";
+            error[0].style.display = "block"
+            click[0].style.display = "block"
+            click1[0].style.display = "none"
+            return false;
+        }
+        else if(input[1].value.trim() == ""){
+            input[1].focus()
+            input[1].style.border = "1px solid red";
+            error[1].style.display = "block"
+            click[1].style.display = "block"
+            click1[1].style.display = "none"
+            return false;
+        }
+        else if(input[2].value.trim() == ""){
+            input[2].focus()
+            input[2].style.border = "1px solid red";
+            error[2].style.display = "block"
+            click[2].style.display = "block"
+            click1[2].style.display = "none"
+            return false;
+        }
+        else if(input[2].value.length < 6){
+            input[2].focus()
+            input[2].style.border = "1px solid red";
+            error[2].style.display = "block"
+            click[2].style.display = "block"
+            click1[2].style.display = "none"
+            error[2].innerHTML = "Password must not be less than 6 characters"
+            return false;
+        }
+        else if(input[3].value.trim() == ""){
+            input[3].focus()
+            input[3].style.border = "1px solid red";
+            error[3].style.display = "block"
+            click[3].style.display = "block"
+            click1[3].style.display = "none"
+            error[3].innerHTML = "Please confirm your Password"
+            return false;
+        }
 
 // git add .
 // git commit -m "Js added"
