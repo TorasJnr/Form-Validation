487a64f720ef36786ce70b59e30358674c24be67
TorasJnr <rasheedidris416@gmail.com>
2026-01-19 22:53:50 +0100

    Initial commit

--- script.js
+++ script.js
@@ -126,9 +126,6 @@ var input = document.querySelectorAll("input");
             hide[1].style.display = "block"
         }
     })
-    hide[3].addEventListener("click", function(){
-        if(input[3].type == "text"){
-            input[3].type = "password"
     hide[1].addEventListener("click", function(){
         if(input[3].type == "text"){
             input[3].type = "password"
@@ -136,6 +133,7 @@ var input = document.querySelectorAll("input");
             hide[1].style.display = "none"
         }
     }) 
+    show 
     
     
 
