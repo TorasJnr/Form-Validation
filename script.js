3136121741c202ed823ae0118f8eaa1b93f2d8ee
TorasJnr <rasheedidris416@gmail.com>
2026-01-19 22:47:21 +0100

    Initial commit: Transforming workflow into product

--- script.js
+++ script.js
@@ -126,6 +126,7 @@ var input = document.querySelectorAll("input");
             hide[1].style.display = "block"
         }
     })
+    hide
     hide[1].addEventListener("click", function(){
         if(input[3].type == "text"){
             input[3].type = "password"
