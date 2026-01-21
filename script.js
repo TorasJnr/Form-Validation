10a7053ed57c250bf9898acc0e3227baa063f752
TorasJnr <rasheedidris416@gmail.com>
2026-01-21 22:13:11 +0100

    Js added

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ var input = document.querySelectorAll("input");
         }
     }) 
     hide[3].style.display = "none"
-    input[1.].style.display
+    input[1.].style.display = none
 //     git init 
 // git add .
 // git commit -m "Initial commit: Transforming workflow into product"
